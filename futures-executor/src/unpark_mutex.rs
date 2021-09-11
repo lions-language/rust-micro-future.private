de57c1e52ce042a47cbc53ab0d8f8ebf602ff535
lions-language <lions_language@163.com>
2021-09-11 21:20:58 +0800

    update

--- futures-executor/src/unpark_mutex.rs
+++ futures-executor/src/unpark_mutex.rs
@@ -5,12 +5,23 @@ pub(crate) struct UnparkMutex<D> {
     inner: UnsafeCell<Option<D>>,
 }
 
+unsafe impl<D: Send> Send for UnparkMutex<D> {}
+unsafe impl<D: Send> Sync for UnparkMutex<D> {}
+
+const WAITING: usize = 0;
+const POLLING: usize = 1;
+const REPOLL: usize = 2;
+
 impl<D> UnparkMutex<D> {
     pub(crate) fn new() -> Self {
         Self {
             status: AtomicUsize::new(WAITING),
             inner: UnsafeCell::new(None)
         }
+    }
+
+    pub(crate) fn notify(&self) -> Result<D, ()> {
+        let mut status = self.status.load(SeqCst);
     }
 }
 
